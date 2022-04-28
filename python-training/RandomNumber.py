210129f06030028ac3e01c9c921c9fbce5e5cc32
Carson Tan <carsonsqlbi@gmail.com>
2022-04-28 21:44:23 +1000

    add a new file RandomNumber

---
+++ python-training/RandomNumber.py
@@ -0,0 +1,7 @@
+import random
+
+lucky_number_1 = random.randint(1, 45)
+
+lucky_number_2 = random.randint(1, 45)
+
+print(f"Your first lucky number is {lucky_number_1} and second lucky number is {lucky_number_2} !")
