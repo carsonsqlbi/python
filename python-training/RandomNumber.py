031533aac6e88911d7b3abcd1dc0b26923104af1
Carson Tan <carsonsqlbi@gmail.com>
2022-04-28 21:48:16 +1000

    add comments

--- python-training/RandomNumber.py
+++ python-training/RandomNumber.py
@@ -1,3 +1,20 @@
+"""
+===============================================================================
+-- Author:		    Carson Tan
+-- Create date:     2022-04-28
+-- Version:         1.0
+-- Story #:         
+-- Description:	    Random Number
+
+-- Change History:
+-- Author:		    
+-- Create date:     
+-- Version:        
+-- Story #:         
+-- Description:     
+===============================================================================
+"""
+
 import random
 
 lucky_number_1 = random.randint(1, 45)
